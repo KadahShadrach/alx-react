5b6aced3207a1ec8a96291b756e64ca3bd0e90cb
KadahShadrach <shadrachaudukadah@yahoo.com>
2024-07-22 19:09:55 +0000

    2. Adding CSS & Images

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -2,19 +2,24 @@ import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
 
-$(document).ready(function() {
-  $('body').prepend('<div id="logo"></div>');
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
+let count = 0;
 
-  let count = 0;
-  function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
-  }
+function updateCounter() {
+    count += 1;
+    return count;
+}
 
-  $('button').on('click', _.debounce(updateCounter, 300));
+$(function() {
+    $('body').append("<div id='logo'></div>")
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button>Click here to get started</button>');
+    $('body').append("<p id='count'></p>");
+    $('body').append("<p>Copyright - Holberton School</p>");
+
+    let debounceFunc = _.debounce(() => {
+        let count = updateCounter();
+        $('#count').text(`${count} clicks on the button`);
+    });
+    $('button').on('click', debounceFunc);
 });
