79cf111b98fdced843b25d2298f40b0758b94473
KadahShadrach <shadracaudukadah@yahoo.com>
2024-08-01 22:11:54 +0100

    0. Commence with class components

--- 0x03-React_component/task_0/dashboard/src/index.js
+++ 0x03-React_component/task_0/dashboard/src/index.js
@@ -1,3 +1,18 @@
-{
-  "presets": ["@babel/preset-env", "@babel/preset-react"]
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+// import Notifications from './Notifications/Notifications';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Notifications />
+//   </React.StrictMode>,
+//   document.getElementById('root-notifications')
+// );
