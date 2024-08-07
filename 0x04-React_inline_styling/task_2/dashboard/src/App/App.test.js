bde05cb26253ef49b16b8a6735172ac01dae2527
KadahShadrach <shadracaudukadah@yahoo.com>
2024-08-07 15:26:42 +0100

    2. Conditionally applying style

--- 0x04-React_inline_styling/task_2/dashboard/src/App/App.test.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/App/App.test.js
@@ -1,60 +1,41 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import App from './App';
-import { StyleSheetTestUtils } from 'aphrodite';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
-describe('<App />', () => {
-  beforeAll(() => {
-    StyleSheetTestUtils.suppressStyleInjection();
-  });
-  afterAll(() => {
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-  });
+class BodySection extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  it('render without crashing', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.exists());
-  });
+  render() {
+    const { children, title } = this.props;
+    return (
+      <div className={css(styles.bodySection)}>
+        <h2 className={css(styles.heading)}>{title}</h2>
+        {children}
+      </div>
+    );
+  }
+}
 
-  it('contain Notifications component', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('Notifications')).toHaveLength(1);
-  });
+BodySection.defaultProps = {
+  title: '',
+};
 
-  it('contain Header component', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('Header')).toHaveLength(1);
-  });
+BodySection.propTypes = {
+  title: PropTypes.string,
+};
 
-  it('contain Login component', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('Login')).toHaveLength(1);
-  });
-
-  it('contain Footer component', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('Footer')).toHaveLength(1);
-  });
-
-  it('CourseList', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('CourseList')).toHaveLength(0);
-  });
-
-  it('isLoggedIn true', () => {
-    const wrapper = shallow(<App isLoggedIn />);
-    expect(wrapper.exists());
-    expect(wrapper.find('Login')).toHaveLength(0);
-    expect(wrapper.find('CourseList')).toHaveLength(1);
-  });
-
-  it('logOut', () => {
-    const logOut = jest.fn(() => undefined);
-    const wrapper = shallow(<App logOut={logOut} />);
-    expect(wrapper.exists());
-    const alert = jest.spyOn(global, 'alert');
-    expect(alert);
-    expect(logOut);
-    jest.restoreAllMocks();
-  });
+const styles = StyleSheet.create({
+  bodySection: {
+    width: '100%',
+    marginTop: '160px',
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  heading: {
+    width: '100%',
+  },
 });
+
+export default BodySection;
